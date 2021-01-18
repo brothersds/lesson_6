ba15d06e98584f4902c3878e9169cea6681729b7
Dmitry <brothersds@ngs.ru>
2021-01-18 23:12:02 +0300

    completed task_3

--- task_6_3.py
+++ task_6_3.py
@@ -7,3 +7,31 @@
 (get_total_income);
 проверить работу примера на реальных данных: создать экземпляры класса Position, передать данные,
 проверить значения атрибутов, вызвать методы экземпляров.'''
+
+
+class Worker:
+    def __init__(self, name, surname, position, income):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = income
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, income):
+        super().__init__(name, surname, position, income)
+
+    def get_full_name(self):
+        return str(self.name + ' ' + self.surname)
+
+    def get_total_income(self):
+        return int(self._income.get('wage')) + int(self._income.get('bonus'))
+
+
+my_position = Position(f'{input("Введите имя сотрудника: ")}',
+                       f'{input("Введите фамилию сотрудника: ")}', f''
+                                                                   f'{input("Введите должность сотрудника: ")}',
+                       {'wage': f'{input("Введите оклад сотрудника: ")}',
+                        'bonus': f'{input("Введите премию сотрудника: ")}'})
+
+print(f'Сотрудник: ', my_position.get_full_name(), f' c доходом = ', my_position.get_total_income(), ' рублей!')
